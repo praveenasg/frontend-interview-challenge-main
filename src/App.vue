b2fe490571fe1584e1fe712c24da941d182ae5fd
praveenasg <praveenasg98@gmail.com>
2025-01-31 19:53:03 -0500

    `Update import statement in App.vue to point to index.vue instead of Home.vue`

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script setup>
 // imported all at once to test the features quickly by eliminating the hastle to route to different page
-// import Home from "./pages/Home.vue";
+// import Home from "./pages/index.vue";
 // import Admin from "./pages/admin.vue";
 // import Cart from "./pages/cart.vue";
 </script>
